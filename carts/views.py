eb63a4345d406a6cbf03c1342a4c266333a2f2d1
joel <joelvarghese880@gmail.com>
2025-11-06 17:44:42 +0530

    Add cart model

--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,45 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from .models import Cart, Cart_item
+from store.models import Product
+
 
 # Create your views here.
-def cart(request):
+
+def _cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+
+def add_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+    except Cart.DoesNotExist: 
+        cart = Cart.objects.create(
+            cart_id=_cart_id(request)
+        )
+        cart.save()
+
+    try:
+        cart_item = Cart_item.objects.get(product=product, cart=cart)
+        cart_item.quantity += 1
+        cart_item.save()
+    except Cart_item.DoesNotExist:
+        cart_item = Cart_item.objects.create(
+               product=product,
+                quantity=1,
+                cart=cart
+            )
+        cart_item.save()
+    return HttpResponse(cart_item.product)
+    exit()
+    return redirect('cart')
+
+
+    
+
+def cart(request, total=0, quantity=0, cart_items=None):
+  
     return render(request, 'store/cart.html')
